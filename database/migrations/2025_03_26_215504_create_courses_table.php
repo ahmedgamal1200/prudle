ba8b18ffe4b35a14d0e068aca04dba50e68ad978
Ahmed Gamal <113640948+ahmedgamal1200@users.noreply.github.com>
2025-03-28 17:09:31 +0200

    deleted column image in every table, her0_section, our_values, partners, blogs, teachers, courses -> tables

--- database/migrations/2025_03_26_215504_create_courses_table.php
+++ database/migrations/2025_03_26_215504_create_courses_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
-            $table->string('image');
             $table->string('slug');
             $table->string('title');
             $table->string('duration');
